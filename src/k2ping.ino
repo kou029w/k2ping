c5d008e988a1550bb0a66a01a7f67368feed236f
kou029w <kou029w@gmail.com>
2017-04-14 21:57:19 +0900

    First commit

---
+++ src/k2ping.ino
@@ -0,0 +1,22 @@
+#include <ESP8266WiFi.h>
+
+// Model: WeMos D1
+#define D0 3
+#define D1 1
+#define D2 16
+#define D3 5
+#define D4 4
+#define D5 14
+#define D6 12
+#define D7 13
+#define D8 0
+#define D9 2
+#define D10 15
+
+void setup() {
+  ;
+}
+
+void loop() {
+  ;
+}
