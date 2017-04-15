7e4193796813cd2e3e0755de3593b4d13e7e09d7
kou029w <kou029w@gmail.com>
2017-04-15 11:56:44 +0900

    Wi-Fiに接続できる

--- src/k2ping.ino
+++ src/k2ping.ino
@@ -4,7 +4,20 @@
 static const uint8_t D[] = {3, 1, 16, 5, 4, 14, 12, 13, 0, 2, 15};
 
 void setup() {
-  ;
+  Serial.begin(9600);
+  Serial.println();
+
+  WiFi.begin("network-name", "pass-to-network");
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
