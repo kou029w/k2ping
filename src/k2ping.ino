438756b0f3383ef22d865577783a426290b732b2
kou029w <kou029w@gmail.com>
2017-04-15 13:06:48 +0900

    LCDにWi-Fi接続状況を表示する

--- src/k2ping.ino
+++ src/k2ping.ino
@@ -1,23 +1,38 @@
 #include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <LiquidCrystal.h>
 
 // Digital pins for WeMos D1
 static const uint8_t D[] = {3, 1, 16, 5, 4, 14, 12, 13, 0, 2, 15};
 
+// LCD Keypad Shield for Arduino
+LiquidCrystal lcd(D[8], D[9], D[4], D[5], D[6], D[7]);
+
 void setup() {
-  Serial.begin(9600);
-  Serial.println();
+  lcd.begin(16, 2);
 
   WiFi.begin("network-name", "pass-to-network");
 
-  Serial.print("Connecting");
+  lcd.print("Connecting");
+  lcd.setCursor(0, 1);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
-  }
-  Serial.println();
 
-  Serial.print("Connected, IP address: ");
-  Serial.println(WiFi.localIP());
+    static uint8_t i = 0;
+    lcd.setCursor((i %= 32) % 16, 1);
+    if (i < 16) {
+      lcd.write(0xFF);
+      i++;
+    } else if (i < 32) {
+      lcd.print(" ");
+      i++;
+    }
+  }
+
+  lcd.clear();
+  lcd.print("Connected");
+  lcd.setCursor(0, 1);
+  lcd.print(WiFi.localIP());
 }
 
 void loop() {
