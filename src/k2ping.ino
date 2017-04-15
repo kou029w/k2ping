c46bf1698507243e74c01b1c1c6a1cacaa12371a
kou029w <kou029w@gmail.com>
2017-04-15 11:37:24 +0900

    スコープを明瞭化

--- src/k2ping.ino
+++ src/k2ping.ino
@@ -1,17 +1,7 @@
 #include <ESP8266WiFi.h>
 
-// Model: WeMos D1
-#define D0 3
-#define D1 1
-#define D2 16
-#define D3 5
-#define D4 4
-#define D5 14
-#define D6 12
-#define D7 13
-#define D8 0
-#define D9 2
-#define D10 15
+// Digital pins for WeMos D1
+static const uint8_t D[] = {3, 1, 16, 5, 4, 14, 12, 13, 0, 2, 15};
 
 void setup() {
   ;
